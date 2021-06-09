c407778ab09d30e53e8cb92d16c0dd92fd719077
tuoilt4 <84076866+tuoilt4@users.noreply.github.com>
2021-06-10 01:48:43 +0700

    Add files via upload

--- BaiTapBuoi13.java
+++ BaiTapBuoi13.java
@@ -1,40 +1,22 @@
-import java.util.Scanner;
+package omg;
+
+import org.junit.Test;
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.assertj.core.api.Assertions.withPrecision;
 
 public class BaiTapBuoi13 {
-    public void chuanhoaten(String ten)
+
+    @Test
+    public void KtManv()
     {
-        Scanner sc= new Scanner(System.in);
-        ten = ten.trim();
-        ten = ten.replaceAll("\\s+"," ");
-        ten = ten.toLowerCase();
-        ten = ten.replace(ten.valueOf(ten.charAt(0)),ten.valueOf(ten.charAt(0)).toUpperCase());
-        for (int i = 0; i < ten.length(); i++) {
-            char kyTu = ten.charAt(i);
-            if (Character.isSpaceChar(kyTu)) {
-                ten = ten.replace(ten.valueOf(ten.charAt(i+1)),ten.valueOf(ten.charAt(i+1)).toUpperCase());
-            }
-        }
-        System.out.print("Họ tên đã chuẩn hóa: "+ten);
+        String manv = "NV332243203";
+        assertThat(manv).isNotEmpty();
+        assertThat(manv).isNotNull();
+        assertThat(manv).hasSize(11);
+        assertThat(manv).startsWith("NV");
+        assertThat(manv).containsPattern("[N][V][\\d]{9}");
 
     }
 
-    public boolean ktrChu (String ten) {
-        for (int i = 0; i < ten.length(); i++) {
-            char kyTu = ten.charAt(i);
-            if (!Character.isLetter(kyTu) & !Character.isSpaceChar(kyTu)){
-                return false;
-            }
-        }
-            return true;
 
-    }
-    public boolean ktrSpace (String ten) {
-        for (int i = 0; i < ten.length(); i++) {
-            char kyTu = ten.charAt(i);
-            if (!Character.isSpaceChar(kyTu)) {
-                return true;
-            }
-        }
-        return false;
-    }
 }
